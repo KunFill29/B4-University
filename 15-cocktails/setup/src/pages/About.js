a08c52b34e41f2f47ae85bfb5205619bbe3bd49c
KunFill29 <fillter.29@gmail.com>
2024-04-27 22:25:43 +0700

    Add all folder

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        lorem asdfmsadlfas;dlfjasdlkfnasd;flknasdfklsandfasf
+        sdfkasdklfaskld'fas'fkl;sadfk'askfkasdfksa'fassdfksadlf
+        sdfkasld;fkasd'l;fkasd';flaskf'as;dlfksd'a;faf
+      </p>
+    </section>
+  )
+}
+
+export default About
